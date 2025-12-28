46346147033331e7debdf958d3bba47416733448
Kami <josephkiarie561@gmail.com>
2025-12-28 21:26:42 +0300

    load data function

--- app/main.py
+++ app/main.py
@@ -22,3 +22,30 @@ Welcome to **DataScope+**, an interactive tool to explore, clean, and visualize 
 Upload your CSV, Excel, or Stata files and get automated insights with interactive charts and EDA.
 """)
 
+# upload
+uploaded_life = st.file_uploader(
+    "Upload Dataset (csv, xlsx, xls, dta)",
+    type = ["csv", "xlsx", "xls", "dta"]
+)
+
+# load data
+def load_data(file):
+    try:
+        if file.name.endswith(".csv"):
+            return pd.read_csv(file)
+        elif file.name.endswith(".xlsx"):
+            xls = pd.ExcelFile(file)
+            sheet = st.selectbox("Select Excel sheet", xls.sheet_names)
+            return pd.read_excel(xls, sheet_name=sheet, engine="openpyxl")
+        elif file.name.endswith(".xls"):
+            xls = pd.ExcelFile(file, engine="xlrd")
+            sheet = st.selectbox("Select Excel sheet", xls.sheet_names)
+            return pd.read_excel(xls, sheet_name=sheet, engine="xlrd")
+        elif file.name.endswith(".dta"):
+            return pd.read_stata(file)
+        else:
+            st.error("Unsupported file format!!!")
+            return None
+    except Exception as e:
+        st.error(f"Error loading file: {e}")
+        return None
