77ac6fb1ad56aeb6cf2b5b06e2d0347bb6fdafcd
Kami <josephkiarie561@gmail.com>
2025-12-28 20:53:30 +0300

    data analyser

--- app/main.py
+++ app/main.py
@@ -10,3 +10,15 @@ import io
 import base64
 
 # app configuration
+st.set_page_config(
+    page_title = "DataScope+",
+    layout = "wide",
+    page_icon = "📈"
+)
+
+st.title("📊 DataScope+ | Interactive Data Analyzer & Cleaner")
+st.markdown("""
+Welcome to **DataScope+**, an interactive tool to explore, clean, and visualize your datasets.
+Upload your CSV, Excel, or Stata files and get automated insights with interactive charts and EDA.
+""")
+
