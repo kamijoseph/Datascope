9057c8006b66134da81ce0ed3df1fe05b3c7fcbe
Kami <josephkiarie561@gmail.com>
2025-12-28 20:36:07 +0300

    app configuration

--- app/main.py
+++ app/main.py
@@ -1,2 +1,12 @@
 
-#
+# dependencies
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import io
+import base64
+
+# app configuration
